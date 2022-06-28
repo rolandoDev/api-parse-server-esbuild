887d0b85af7f67b56cc8fae7c40ddf7f69045a47
rolando <rolando.sanchez.developer@gmail.com>
2022-06-28 00:59:19 -0400

    add parse-dashboard to route /dashboard

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,60 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
-const express =  require("express");
+const express = require("express");
 const ParseServer = require("parse-server").ParseServer;
-const { version } = require('package.json');
+const { version } = require("package.json");
 const path = require("path");
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
+const ParseDashboard = require("parse-dashboard");
+const logger = require('parse-server').logger;
 const config = {
   databaseURI: process.env.DATABASE_URI,
-  cloud: path.join(__dirname, '/cloud/main.js'),
+  cloud: path.join(__dirname, "/cloud/main.js"),
   appId: process.env.APP_ID,
   masterKey: process.env.MASTER_KEY, //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL, // Don't forget to change to https if needed
   liveQuery: {
     classNames: [], // List of classes to support for query subscriptions
   },
-  silent: true,
+  allowClientClassCreation: false,
 };
-
+const dashboard = new ParseDashboard({
+  apps: [
+    {
+      serverURL: process.env.SERVER_URL,
+      appId: process.env.APP_ID,
+      masterKey: process.env.MASTER_KEY,
+      appName: "Dashboard",
+    },
+  ],
+  users: [
+    {
+      user: process.env.DASHBOARD_USER,
+      pass: process.env.DASHBOARD_PASSWORD,
+    },
+  ],
+  useEncryptedPasswords: false,
+});
 const app = express();
 
 // Serve static assets from the /public folder
 // app.use('/public', express.static(path.join(__dirname, '/public')));
 const api = new ParseServer(config);
 app.use("/api", api);
-
+if (process.env.NODE_ENV === "development") {
+  app.use("/dashboard", dashboard);
+}
 // Parse Server plays nicely with the rest of your web routes
-app.get("/", (_, res) =>{
-  res
-    .status(200)
-    .send(
-      `API version: ${version}`
-    );
+app.get("/", (_, res) => {
+  res.status(200).send(`API version: ${version}`);
 });
 
 const port = process.env.PORT;
-
-  const httpServer = require("http").createServer(app);
-  httpServer.listen(port,  ()=> {
-    console.log("parse-server-example running on port " + port + ".");
-  });
-  // This will enable the Live Query real-time server
-  ParseServer.createLiveQueryServer(httpServer);
+const httpServer = require("http").createServer(app);
+httpServer.listen(port, () => {
+  logger.info("parse-server-example running on port " + port + ".");
+});
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
